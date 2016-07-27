a3a0c6f30de0dcf2ff65d3e713ec8a91c156137c
Karl Penzhorn <karlpenzhorn@gmail.com>
2016-07-27 19:59:21 +0200

    Committing article code

---
+++ src/main/java/demo/PersonRepository.java
@@ -0,0 +1,12 @@
+package demo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
